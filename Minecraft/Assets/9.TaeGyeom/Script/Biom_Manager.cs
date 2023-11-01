5aeea680fb1eb5d65912119769733ca779182a6c
jas03006 <jas03006@kaist.ac.kr>
2023-11-01 17:14:28 +0900

    [repack] 지형 자동생성 최적화 중
    block open check: open된 블럭만 렌더링,
    자동 생성 코루틴화

--- Minecraft/Assets/9.TaeGyeom/Script/Biom_Manager.cs
+++ Minecraft/Assets/9.TaeGyeom/Script/Biom_Manager.cs
@@ -2,61 +2,164 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Item_ID_TG { 
+    None = 0,
+    stone= 1,
+    grass,
+    dirt,
+    coal = 15
+}
+
 public class Biom_Manager : MonoBehaviour
 {
     public static Biom_Manager instance = null;
     [SerializeField] private Vector3Int start_chunk_pos = Vector3Int.zero;
     [SerializeField] public int chunk_size;
     [SerializeField] public int render_distance;
-    [SerializeField] public List<GameObject> block_prefab_list;
+    private int render_chunk_num = 1;
+    private Vector3Int current_chunk_pos = Vector3Int.zero;
+    [SerializeField] private float chunk_update_time = 1f;
+    private float chunk_update_timer = 0f;
+    private Coroutine now_update_co = null;
+    //[SerializeField] public Dictionary<Item_ID_TG, GameObject> block_prefab_dict;
+    [SerializeField] public ID2Block_TG block_prefabs_SO;
     [SerializeField] public GameObject chunk_prefab;
     [SerializeField] private GameObject player;
+
+    private Dictionary<Vector3Int, Chunk_TG> chunk_data;
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
         }
-        else {
+        else
+        {
             Destroy(gameObject);
             return;
         }
+        
+        render_chunk_num = render_distance / chunk_size;
         update_start_pos();
+        
     }
 
     private void Start()
     {
+        chunk_data = new Dictionary<Vector3Int, Chunk_TG>();
         generate_start_map();
     }
-    private void update_start_pos() {
+
+    private void Update()
+    {
+        chunk_update_timer += Time.deltaTime;
+        if (now_update_co == null && chunk_update_timer > chunk_update_time)
+        {
+            chunk_update_timer = 0f;
+            current_chunk_pos = get_player_chunk_pos();
+            now_update_co = StartCoroutine(generate_map_update_co());
+        }
+
+    }
+    private void update_start_pos()
+    {
         //Vector3Int temp_pos = new Vector3Int(start_pos.x, start_pos.y-1,start_pos.z);
         player = GameObject.FindGameObjectWithTag("Player");
-        if (player != null) {
-            start_chunk_pos = world2chunk_pos(player.transform.position);
+        if (player != null)
+        {
+            current_chunk_pos = get_player_chunk_pos();
         }
-        
+
     }
 
-    private void generate_start_map() {
-        int render_chunk_num = render_distance / chunk_size;
+    private void generate_start_map()
+    {
+        Chunk_TG new_chunk;
         Vector3Int now_chunk_pos = Vector3Int.zero;
-        for (int i = start_chunk_pos.x - render_chunk_num; i < start_chunk_pos.x + render_chunk_num; i++) {
+        for (int i = start_chunk_pos.x - render_chunk_num; i < start_chunk_pos.x + render_chunk_num; i++)
+        {
             for (int j = start_chunk_pos.y - 1; j < start_chunk_pos.y + 1; j++)
             {
-                for (int k = start_chunk_pos.z- render_chunk_num; k < start_chunk_pos.z + render_chunk_num; k++)
+                for (int k = start_chunk_pos.z - render_chunk_num; k < start_chunk_pos.z + render_chunk_num; k++)
                 {
                     now_chunk_pos.x = i;
                     now_chunk_pos.y = j;
                     now_chunk_pos.z = k;
-                    Chunk_TG new_chunk = Instantiate(chunk_prefab, chunk2world_pos(now_chunk_pos), Quaternion.identity).GetComponent<Chunk_TG>();
+                    new_chunk = Instantiate(chunk_prefab, chunk2world_pos(now_chunk_pos), Quaternion.identity).GetComponent<Chunk_TG>();
                     new_chunk.transform.SetParent(transform);
-                    new_chunk.init(chunk_size, now_chunk_pos);                    
-                    new_chunk.generate_blocks();
+                    new_chunk.init(chunk_size, now_chunk_pos);
+                    chunk_data[now_chunk_pos] = new_chunk;
+                    new_chunk.generate_blocks();                    
                 }
             }
-        }        
+        }
+
+        for (int i = current_chunk_pos.x - render_chunk_num; i < current_chunk_pos.x + render_chunk_num; i++)
+        {
+            for (int j = current_chunk_pos.y - 1; j < current_chunk_pos.y + 1; j++)
+            {
+                for (int k = current_chunk_pos.z - render_chunk_num; k < current_chunk_pos.z + render_chunk_num; k++)
+                {
+                    now_chunk_pos.x = i;
+                    now_chunk_pos.y = j;
+                    now_chunk_pos.z = k;
+                    new_chunk = get_chunk(now_chunk_pos);
+                    if (!new_chunk.is_open_checked)
+                    {
+                        new_chunk.check_open_and_show_all();
+             
+                    }
+                }
+            }
+        }
     }
-    
+
+    private IEnumerator generate_map_update_co() {
+        Vector3Int now_chunk_pos = Vector3Int.zero;
+        Chunk_TG new_chunk;
+        for (int i = current_chunk_pos.x - render_chunk_num; i < current_chunk_pos.x + render_chunk_num; i++)
+        {
+            for (int j = current_chunk_pos.y - 1; j < current_chunk_pos.y + 1; j++)
+            {
+                for (int k = current_chunk_pos.z - render_chunk_num; k < current_chunk_pos.z + render_chunk_num; k++)
+                {
+                    now_chunk_pos.x = i;
+                    now_chunk_pos.y = j;
+                    now_chunk_pos.z = k;
+                    if (get_chunk(now_chunk_pos) == null) {
+                        new_chunk = Instantiate(chunk_prefab, chunk2world_pos(now_chunk_pos), Quaternion.identity).GetComponent<Chunk_TG>();
+                        new_chunk.transform.SetParent(transform);
+                        new_chunk.init(chunk_size, now_chunk_pos);
+                        chunk_data[now_chunk_pos] = new_chunk;
+                        
+                        yield return new_chunk.generate_blocks_co();
+                    }                    
+                }
+            }
+        }
+
+        for (int i = current_chunk_pos.x - render_chunk_num; i < current_chunk_pos.x + render_chunk_num; i++)
+        {
+            for (int j = current_chunk_pos.y - 1; j < current_chunk_pos.y + 1; j++)
+            {
+                for (int k = current_chunk_pos.z - render_chunk_num; k < current_chunk_pos.z + render_chunk_num; k++)
+                {
+                    now_chunk_pos.x = i;
+                    now_chunk_pos.y = j;
+                    now_chunk_pos.z = k;
+                    new_chunk = get_chunk(now_chunk_pos);
+                    if (!new_chunk.is_open_checked) {
+                        new_chunk.check_open_and_show_all();
+                        yield return null;
+                    }                       
+                }
+            }
+        }
+
+        now_update_co = null;
+    }
+
+
 
     public Vector3 chunk2world_pos(Vector3Int chunk_pos) {
         return chunk_pos * chunk_size;
@@ -72,4 +175,26 @@ public class Biom_Manager : MonoBehaviour
         return pos;
     }
 
+    public Chunk_TG get_chunk(Vector3Int chunk_pos) {
+        if (chunk_data.ContainsKey(chunk_pos)) {
+            return chunk_data[chunk_pos];
+        }
+        return null;
+    }
+
+    public Block_Node_TG get_block(Vector3Int chunk_pos, Vector3Int block_pos) {
+        chunk_pos.x += (block_pos.x) / chunk_size; 
+        chunk_pos.y += (block_pos.y) / chunk_size; 
+        chunk_pos.z += (block_pos.z) / chunk_size;
+        Chunk_TG ch = get_chunk(chunk_pos);
+        if (ch == null) {
+            return null;
+        }
+        return ch.block_data[(block_pos.x+chunk_size)%chunk_size, (block_pos.y + chunk_size) % chunk_size, (block_pos.z + chunk_size) % chunk_size];
+    }
+
+    private Vector3Int get_player_chunk_pos() {
+
+        return world2chunk_pos(player.transform.position);
+    }
 }
--- Minecraft/Assets/9.TaeGyeom/Script/Chunk_TG.cs
+++ Minecraft/Assets/9.TaeGyeom/Script/Chunk_TG.cs
@@ -7,6 +7,7 @@ public class Chunk_TG: MonoBehaviour
     public int chunk_size;
     public Block_Node_TG[,,] block_data;
     public Vector3Int chunk_pos;
+    public bool is_open_checked = false;
     // Start is called before the first frame update
     
     public void init(int chunk_size_, Vector3Int chunk_pos_)
@@ -24,21 +25,45 @@ public class Chunk_TG: MonoBehaviour
             {
                 for (int k =0; k < chunk_size; k++)
                 {
-                    int temp_index = get_prefabs_index(i,j,k);
-                    if (temp_index == -1) {
+                    Item_ID_TG temp_index = get_prefabs_index(i,j,k);
+                    if (temp_index == Item_ID_TG.None) {
                         continue;
                     }
-                    GameObject go = GameObject.Instantiate(Biom_Manager.instance.block_prefab_list[temp_index], new Vector3(origin_pos.x+i, origin_pos.y+j, origin_pos.z+ k), Quaternion.identity);
+                    GameObject go = GameObject.Instantiate(Biom_Manager.instance.block_prefabs_SO.get_prefab(temp_index), new Vector3(origin_pos.x+i, origin_pos.y+j, origin_pos.z+ k), Quaternion.identity);
                     go.transform.SetParent(transform);
                     block_data[i, j , k] = go.GetComponent<Block_Node_TG>();
                     block_data[i, j, k].set_local_pos(i,j,k);
                 }
+                
             }
         }
-        check_open_and_show_all();
+       // check_open_and_show_all();
     }
-
-    private void check_open_and_show_all() {
+    public IEnumerator generate_blocks_co()
+    {
+        Vector3Int origin_pos = Biom_Manager.instance.chunk2world_pos_int(chunk_pos);
+        for (int i = 0; i < chunk_size; i++)
+        {
+            for (int j = 0; j < chunk_size; j++)
+            {
+                for (int k = 0; k < chunk_size; k++)
+                {
+                    Item_ID_TG temp_index = get_prefabs_index(i, j, k);
+                    if (temp_index == Item_ID_TG.None)
+                    {
+                        continue;
+                    }
+                    GameObject go = GameObject.Instantiate(Biom_Manager.instance.block_prefabs_SO.get_prefab(temp_index), new Vector3(origin_pos.x + i, origin_pos.y + j, origin_pos.z + k), Quaternion.identity);
+                    go.transform.SetParent(transform);
+                    block_data[i, j, k] = go.GetComponent<Block_Node_TG>();
+                    block_data[i, j, k].set_local_pos(i, j, k);
+                }                
+            }
+            yield return null;
+        }
+        //check_open_and_show_all();
+    }
+    public void check_open_and_show_all() {
         for (int i = 0; i < block_data.GetLength(0); i++)
         {
             for (int j = 0; j < block_data.GetLength(1); j++)
@@ -55,13 +80,15 @@ public class Chunk_TG: MonoBehaviour
                 }
             }
         }
+        is_open_checked = true;
     }
 
     public void destory_and_show_adjacents(int x, int y, int z) {
         int[] dir = { -1, 1 };
         block_data[x, y, z] = null;
-        for (int i = 0; i < dir.Length; i++)
-        {            
+        for (int i_ = 0; i_ < dir.Length; i_++)
+        {
+            int i = dir[i_];
             if (is_in_range(x + i, y , z ) && block_data[x + i, y , z] != null)
             {
                 block_data[x + i, y, z ].show();
@@ -81,8 +108,9 @@ public class Chunk_TG: MonoBehaviour
     {
         int[] dir = { -1, 1 };
         block_data[x, y, z] = bn;
-        for (int i = 0; i < dir.Length; i++)
+        for (int i_ = 0; i_ < dir.Length; i_++)
         {
+            int i = dir[i_];
             if (is_in_range(x + i, y , z) && block_data[x + i, y, z ] != null && is_open(x + i, y , z))
             {
                 block_data[x + i, y , z].hide();
@@ -100,17 +128,42 @@ public class Chunk_TG: MonoBehaviour
 
     public bool is_open(int x, int y, int z) {
         int[] dir = {-1,  1 };
-        for (int i = 0; i < dir.Length; i++)
+        for (int i_ = 0; i_ < dir.Length; i_++)
         {
-            if (!is_in_range(x + i, y, z) || block_data[x + i, y, z] == null || !block_data[x + i, y, z].is_blocking)
+            int i = dir[i_];
+            Block_Node_TG bn;
+            if (!is_in_range(x + i, y, z)) {
+                bn = Biom_Manager.instance.get_block(chunk_pos, new Vector3Int(x + i, y, z));
+                if (bn == null || !bn.is_blocking) {
+                    return true;
+                }
+            }
+            else if (block_data[x + i, y, z] == null || !block_data[x + i, y, z].is_blocking)
             {
                 return true;
             }
-            if (!is_in_range(x, y + i, z) || block_data[x, y+i, z] == null || !block_data[x, y+i, z].is_blocking)
+
+            if (!is_in_range(x, y + i, z))
+            {
+                bn = Biom_Manager.instance.get_block(chunk_pos, new Vector3Int(x, y + i, z));
+                if (bn == null || !bn.is_blocking)
+                {
+                    return true;
+                }
+            }
+            else if (block_data[x, y + i, z] == null || !block_data[x, y + i, z].is_blocking)
             {
                 return true;
             }
-            if (!is_in_range(x, y, z + i) || block_data[x , y, z + i] == null || !block_data[x, y, z + i].is_blocking)
+            if (!is_in_range(x, y, z + i))
+            {
+                bn = Biom_Manager.instance.get_block(chunk_pos, new Vector3Int(x, y, z + i));
+                if (bn == null || !bn.is_blocking)
+                {
+                    return true;
+                }
+            }
+            else if( block_data[x , y, z + i] == null || !block_data[x, y, z + i].is_blocking)
             {
                 return true;
             }
@@ -127,11 +180,15 @@ public class Chunk_TG: MonoBehaviour
         return true;
     }
 
-    private int get_prefabs_index(int x, int y, int z) {
-        if (Biom_Manager.instance.chunk2world_pos_int(chunk_pos).y  + y >= 0) {
-            return -1;
+    private Item_ID_TG get_prefabs_index(int x, int y, int z) {
+        int chunk_world_y = Biom_Manager.instance.chunk2world_pos_int(chunk_pos).y;
+        if (chunk_world_y + y >= 0) {
+            return Item_ID_TG.None;
         }
-        return 0;
+        if (chunk_world_y + y== -1) {
+            return Item_ID_TG.grass;
+        }
+        return Item_ID_TG.dirt;
     }
 
 }
