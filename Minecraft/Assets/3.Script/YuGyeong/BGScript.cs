7b230c9ee9160739f62bee9f7d090ea4e3481505
Limkwan1004 <128816863+Hansiban@users.noreply.github.com>
2023-11-02 10:10:07 +0900

    [feat] 플레이어 이동 시스템 구현완료
    점프 구현 완료
    움직임 로직 수정 중

---
+++ Minecraft/Assets/3.Script/YuGyeong/BGScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScript : MonoBehaviour
+{
+    /*
+     테스트용 스크립트
+    (블럭 찾을때 쓰려고 만듬)
+     
+     */
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Minecraft/Assets/3.Script/YuGyeong/PlayerMovement.cs
+++ Minecraft/Assets/3.Script/YuGyeong/PlayerMovement.cs
@@ -20,51 +20,88 @@ public class PlayerMovement : MonoBehaviour
         //Update
             //if(ray 충돌체), 플레이어 입력받기
         //플레이어 입력(Input.GetAxis, jump(addforce))
-        //레이캐스트
+        //속도 설정
 
     //추가로 해야할 것
         //애니메이션 : 웅크리기,뛰기,걷기
         //카메라 모션 : 뛸때 카메라 살짝 멀어지기, 웅크릴때 카메라 살짝 가까워지기
+        //낙사 데미지 구현
      */
     #endregion
-    [Header("속도")]
+
+    private Rigidbody rigid;
+    private Transform cam;
+
+    [Header("Position")]
     [SerializeField] private float crouchspeed;
     [SerializeField] private float walkspeed;
     [SerializeField] private float runspeed;
     private float currentspeed;
+    private Vector3 velocity;
+
+    [SerializeField] private float jumpforce;
+    [SerializeField] private bool isjump;
 
     private float horizontal;
     private float vertical;
-    private float jumpforce;
     private Vector3 moveVec;
 
-    private Rigidbody rigid;
+    [Header("Rotation")]
+    private float mouseX;
+    private float mouseY;
+    [SerializeField]private float r_speed;
 
-    private void Awake()
+    private void Start()
     {
         TryGetComponent(out rigid);
+        cam = GameObject.Find("Main Camera").transform;
 
-        //speed설정
+        isjump = false;
+        jumpforce = 15f;
         crouchspeed = 1f;
         walkspeed = 5f;
         runspeed = 10f;
+        r_speed = 1f;
         currentspeed = walkspeed;
     }
     void Update()
     {
-        if (DrawRay())
-        {
-            PlayerInput();
-        }
+        PositionInput();
+        RotationInput();
+        velocity = (transform.forward * vertical);
     }
-    private void PlayerInput()
+    private void PositionInput()
     {
+        //움직임 속도 조절
         SetSpeed();
+        
+        //x,z축 움직임
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
 
         moveVec = new Vector3(horizontal, 0, vertical) * currentspeed * Time.deltaTime;
         transform.position += moveVec;
+
+        //y축 움직임
+        if (!isjump && Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("점프");
+            isjump = true;
+            rigid.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+        }
+    }
+    private void RotationInput()
+    {
+        //mouseX = Input.GetAxis("Mouse X");
+        //mouseY = Input.GetAxis("Mouse Y");
+        //Debug.Log(mouseX + "/" +mouseY);
+        transform.Rotate(Vector3.up,mouseX * r_speed);
+    }
+    private void CamSet()
+    {
+        cam.Rotate(Vector3.right * -mouseY);
     }
     private void SetSpeed()
     {
@@ -91,17 +128,11 @@ public class PlayerMovement : MonoBehaviour
         }
 
     }
-    private bool DrawRay()
+    private void OnCollisionEnter(Collision col)
     {
-        RaycastHit ray = new RaycastHit();
-        //Vector3 origin, Vector3 direction, out RaycastHit hitInfo, float maxDistance, int layerMask
-        if (Physics.Raycast(transform.position + Vector3.down, Vector3.down,out ray, 1f))
-        { Debug.Log("ray 있음"); return true;  }
-        else
-        { Debug.Log("ray 없음"); return false; }
-    }
-    private void OnDrawGizmos()
-    {
-        //Debug.DrawRay(transform.position + Vector3.down,Vector3.down,Color.red,1f);
+        if (col.gameObject.name == "BG")
+        {
+            isjump = false;
+        }
     }
 }
