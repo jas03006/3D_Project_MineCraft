afc2dece5e4cdbce6f0093cafee2e5daba14d7f4
jas03006 <jas03006@kaist.ac.kr>
2023-11-04 12:49:55 +0900

    [Feat] Item_ID_TG 완성

--- Minecraft/Assets/9.TaeGyeom/Script/ID2Block_TG.cs
+++ Minecraft/Assets/9.TaeGyeom/Script/ID2Block_TG.cs
@@ -7,12 +7,32 @@ using UnityEngine;
 public class ID2Block_TG : ScriptableObject
 {
     [SerializeField] public List<GameObject> block_prefab_list;
-    Dictionary<Item_ID_TG, int> ID2index_dict;
+    private Dictionary<Item_ID_TG, int> ID2index_dict;
+    [SerializeField]
+    public List<Item_ID_TG> block_item_id_list =
+        new List<Item_ID_TG>() { 
+            Item_ID_TG.None,
+            Item_ID_TG.stone,
+            Item_ID_TG.grass,
+            Item_ID_TG.dirt,
+            Item_ID_TG.board,
+            Item_ID_TG.bedrock,
+            Item_ID_TG.coal,
+            Item_ID_TG.iron,            
+            Item_ID_TG.tree,
+            Item_ID_TG.leaf,
+            Item_ID_TG.diamond,
+            Item_ID_TG.box,
+            Item_ID_TG.craft_box,
+            Item_ID_TG.furnace,
+            Item_ID_TG.door,
+            Item_ID_TG.bed
+        };
     public ID2Block_TG() {
 
         ID2index_dict = new Dictionary<Item_ID_TG, int>();
         int i = 0;
-        foreach(Item_ID_TG e in Enum.GetValues(typeof(Item_ID_TG)) ) {
+        foreach(Item_ID_TG e in block_item_id_list){//Enum.GetValues(typeof(Item_ID_TG)) ) {
             ID2index_dict[e] = i;
             //Debug.Log($"{e}, {i}");
             i++;            
@@ -20,8 +40,11 @@ public class ID2Block_TG : ScriptableObject
         
     }
     public GameObject get_prefab(Item_ID_TG id) {
-        
-        return block_prefab_list[ID2index(id)];
+        int ind = ID2index(id);
+        if (block_prefab_list.Count <= ind) {
+            return null;
+        }
+        return block_prefab_list[ind];
     }
 
     public int ID2index(Item_ID_TG id) {        
